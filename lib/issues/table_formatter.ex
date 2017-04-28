660c8fffa03c2e1b13c8408d8e59735cc21d863d
Matija <matija.krajnik90@gmail.com>
2017-04-28 23:32:10 +0200

    headers printing added

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -1,6 +1,7 @@
 defmodule Issues.TableFormatter do  
   def display_result(data, columns_for_display) do
     column_widths = calculate_column_widths(data, columns_for_display)
+    print_headers(columns_for_display, column_widths)
   end
   
   defp calculate_column_widths(data, columns_for_display) do
@@ -14,5 +15,18 @@ defmodule Issues.TableFormatter do
     |>String.length))[key]
     |>to_string
     |>String.length
+  end
+  
+  defp print_headers(columns, widths) do
+    (for c <- columns do
+      " " <> String.replace(c, "number", "#") <> String.duplicate(" ", (widths[c] - String.length(String.replace(c, "number", "#")) + 1))
+    end
+    |>Enum.join("|")
+    |>String.trim_trailing) <> "\n" <>
+    (for c <- columns do
+      String.duplicate("-", (widths[c]) + 1)
+    end
+    |>Enum.join("+-"))
+    |>IO.puts
   end
 end
