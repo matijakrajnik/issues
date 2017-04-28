ec9905850adc8c9bc3e37676b958aaf226603c3e
Matija <matija.krajnik90@gmail.com>
2017-04-28 22:35:02 +0200

    calculation of column widths fixed

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -4,11 +4,15 @@ defmodule Issues.TableFormatter do
   end
   
   defp calculate_column_widths(data, columns_for_display) do
-    for column <- columns_for_display, do: (%{column => find_max_length(data, column)})
-    |>Enum.reduce(fn(x, acc) -> Map.merge(x, acc) end)
+    list_of_maps = for column <- columns_for_display, do: (%{column => find_max_length(data, column)})
+    Enum.reduce(list_of_maps, fn(x, acc) -> Map.merge(x, acc) end)
   end
   
   defp find_max_length(data, key) do
-    Enum.max_by(data, &(&1[key]|>to_string|>String.length))[key]|>to_string|>String.length
+    Enum.max_by(data, &(&1[key]
+    |>to_string
+    |>String.length))[key]
+    |>to_string
+    |>String.length
   end
 end
