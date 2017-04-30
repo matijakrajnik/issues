84918dc90d298737b786a42f9b191a4cee572a00
Matija <matija.krajnik90@gmail.com>
2017-04-30 17:18:14 +0200

    added data printing

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -2,6 +2,7 @@ defmodule Issues.TableFormatter do
   def display_result(data, columns_for_display) do
     column_widths = calculate_column_widths(data, columns_for_display)
     print_headers(columns_for_display, column_widths)
+    print_data(data, columns_for_display, column_widths)
   end
   
   defp calculate_column_widths(data, columns_for_display) do
@@ -19,14 +20,29 @@ defmodule Issues.TableFormatter do
   
   defp print_headers(columns, widths) do
     (for c <- columns do
-      " " <> String.replace(c, "number", "#") <> String.duplicate(" ", (widths[c] - String.length(String.replace(c, "number", "#")) + 1))
+      String.pad_trailing(String.replace(c, "number", "#"), (widths[c]))
     end
-    |>Enum.join("|")
+    |>Enum.join(" | ")
     |>String.trim_trailing) <> "\n" <>
     (for c <- columns do
       String.duplicate("-", (widths[c]) + 1)
     end
     |>Enum.join("+-"))
     |>IO.puts
+  end
+  
+  defp print_data(data, columns, column_widths) do
+    for d <- data do
+      create_one_line(d, columns, column_widths)
+    end
+    |>Enum.join("\n")
+    |>IO.puts
+  end
+  
+  defp create_one_line(data, columns, column_widths) do
+    for c <- columns do
+      String.pad_trailing(to_string(data[c]), column_widths[c])
+    end
+    |>Enum.join(" | ")
   end
 end
