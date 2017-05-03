5dcaaa7f966bc7952bbd17a2984336c9110358a2
Matija <matija.krajnik90@gmail.com>
2017-05-03 16:08:36 +0200

    logging added

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,11 @@
 defmodule Issues.GithubIssues do
+  require Logger
+  
   @user_agent [ {"User-agent", "Elixir matija.krajnik@tentamen.hr"} ]
   @github_url Application.get_env(:issues, :github_url)
   
   def fetch(user, project) do
+    Logger.info("Fetching data for #{user}'s project #{project}")
     issues_url(user, project)
     |>HTTPoison.get(@user_agent)
     |>handle_response
@@ -13,10 +16,14 @@ defmodule Issues.GithubIssues do
   end
   
   def handle_response( {:ok, %{status_code: 200, body: body}} ) do
+    Logger.info("Successful response")
+    Logger.debug(inspect(body))
     { :ok,    Poison.Parser.parse!(body) }
   end
   
-  def handle_response( {  _, %{status_code:   _, body: body}} ) do
+  def handle_response( {  _, %{status_code: status_code, body: body}} ) do
+    Logger.info("Error response with status code #{status_code}")
+    Logger.debug(inspect(body))
     { :error, Poison.Parser.parse!(body) }
   end
 end
