deb35412a1b4a089d2268f4a49b0bb54147a9542
Matija Krajnik <matija.krajnik90@gmail.com>
2017-05-07 19:34:30 +0200

    new exercise task

---
+++ exercise/14_MultipleProcesses/basic_process.exs
@@ -0,0 +1,21 @@
+defmodule BasicProcess do
+  def send_back do
+    receive do
+      {sender, message} -> send(sender, message)
+      after 500 -> IO.puts "Timeout"
+    end
+  end
+end
+
+process1 = spawn(BasicProcess, :send_back, [])
+process2 = spawn(BasicProcess, :send_back, [])
+
+send(process1, {self, :tom})
+send(process2, {self, :jerry})
+
+receive do
+  message -> IO.puts(message)
+end
+receive do
+  message -> IO.puts(message)
+end
