fe552d5b273ad6ca9a8988c38d79e4a5c8e8ce5c
Matija Krajnik <matija.krajnik90@gmail.com>
2017-05-07 21:21:52 +0200

    new exercises

---
+++ exercise/14_MultipleProcesses/basic_monitor.exs
@@ -0,0 +1,22 @@
+defmodule BasicMonitor do
+  def send_pid(parent) do
+    send(parent, "Hello my parent")
+    exit(:boom)
+  end
+  
+  def receive_messages do
+    receive do
+      message -> IO.puts message
+      after 1000 -> IO.puts "Timeout"
+    end
+    receive_messages
+  end
+  
+  def run do
+    spawn_monitor(BasicMonitor, :send_pid, [self()])
+    :timer.sleep(5000)
+    receive_messages
+  end
+end
+  
+BasicMonitor.run
